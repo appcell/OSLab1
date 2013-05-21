2e08b66ef7b49a412e3123dd4a44ffed6d462d63
appcell <eilischu527@gmail.com>
2013-05-21 15:48:11 +0800

    modified main.c to add some test cases

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -10,6 +10,8 @@ os_init(void) {
 	init_idt();
 	init_i8259();
 	printk("The OS is now working!\n");
+	vfprintf("This is a common string.\n");
+	vfprintk("This is a string with an integer num: %d.\n",3);
 	sti();
 	while (TRUE) {
 		wait_intr();
