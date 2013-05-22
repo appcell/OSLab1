71ac0780c2970b362b5032beb30406084c93c88f
appcell <eilischu527@gmail.com>
2013-05-22 14:18:13 +0800

    finished :-)

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -11,13 +11,6 @@ os_init(void) {
 	init_idt();
 	init_i8259();
 	printk("The OS is now working!\n");
-	printk("This is a common string.\n");
-	printk("This is a string with an integer num: %d.\n",3);
-printk("This is a string with an x num valued 335: %x.\n",335);
-printk("This is a string with two nums: %d and %x.\n",3,334);
-printk("This is a string with a string: %s.\n","hello world");
-printk("This is a string with a char: %c.\n",aaa);
-printk("This is a string with a %");
 	sti();
 	while (TRUE) {
 		wait_intr();
